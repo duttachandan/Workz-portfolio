8e025cb1fb306b609e6a94030e1a1e1eda24cf20
duttachandan <duttaankit917@gmail.com>
2025-08-12 10:46:11 +0530

    update Footer Section

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import FooterCSS from "../../styles/footer.module.css";
 import { FaGithub } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
-import useFancybox from "../../components/Fancybox/Fancybox"
-
+import useFancybox from "../../components/Fancybox/Fancybox";
+import { Link } from "react-router";
 
 const Footer = () => {
   const [fancyboxRef] = useFancybox({
@@ -27,7 +27,7 @@ const Footer = () => {
           <div className={`container ${FooterCSS.footerBottom}`}>
             <div className={` ${FooterCSS.bottomwrpr}`}>
               <div className="row">
-                <div className="col-50">
+                <div className="col-40">
                   <div className="links">
                     <div className={`${FooterCSS.footerLinks}`}>
                       <a href="">
@@ -47,24 +47,69 @@ const Footer = () => {
                       </a>
                     </div>
                     <ul className={`${FooterCSS.footerInLinks}`}>
-                      <li><a href="">Home</a></li>
-                      <li><a href="">About</a></li>
-                      <li><a href="">Projects</a></li>
-                      <li><a href="">Journal</a></li>
+                      <li>
+                        <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                        <Link to="/about">About</Link>
+                      </li>
+                      <li>
+                        <Link to="/projects">Projects</Link>
+                      </li>
+                      <li>
+                        <Link to="/contact">Contact</Link>
+                      </li>
                     </ul>
                   </div>
                 </div>
-                <div className="col-50">
-                  <div ref={fancyboxRef}>
-                    <a
-                      href="https://www.google.com/maps/search/?api=1&query=Westminster+Bridge%2C+London&query_place_id=ChIJoaH8k8YEdkgRnmDd5iuUl9Y"
-                      data-fancybox
-                    >
-                      Launch Map
-                    </a>
+                <div className={`col-60 ${FooterCSS.ContactUs} `}>
+                  <div className="row">
+                    <div className="col-50">
+                      <div ref={fancyboxRef}>
+                        <a
+                          href="https://www.google.com/maps/search/?api=1&query=Westminster+Bridge%2C+London&query_place_id=ChIJoaH8k8YEdkgRnmDd5iuUl9Y"
+                          data-fancybox
+                          className={`${FooterCSS.MapDirection}`}
+                        >
+                          Map Direction
+                        </a>
+                        <div>I am available Here</div>
+                      </div>
+
+                      <div>
+                        <a
+                          href="tel:8372068294"
+                          className={`${FooterCSS.MapDirection}`}
+                        >
+                          (+91) 8372068294
+                        </a>
+                        <div>Contact Me At</div>
+                      </div>
+                    </div>
+                    <div className="col-50">
+                      <div>
+                        <a
+                          href="mailto:duttaankit917@gmail.com"
+                          className={`${FooterCSS.MapDirection}`}
+                        >
+                          Duttaankit917@gmail.com
+                        </a>
+                        <div>Email Me Through</div>
+                      </div>
+                      <div>
+                        <div className={`${FooterCSS.MapDirection}`}>
+                          09.30 AM - 8.30 PM
+                        </div>
+                        <div>My Available Timing</div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
+            </div>
+
+            <div className={`${FooterCSS.copyright}`}>
+              <Link to="/">Copyright © 2025 WDT Workz</Link>
             </div>
           </div>
         </div>
