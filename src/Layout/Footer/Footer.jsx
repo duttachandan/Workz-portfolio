063d9261a143bcbb72cc1c5b6e972b7b598e54f6
Chandan Dutta <duttaankit917@gmail.com>
2025-08-12 06:06:10 +0530

    update footer section

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -3,8 +3,13 @@ import FooterCSS from "../../styles/footer.module.css";
 import { FaGithub } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
+import useFancybox from "../../components/Fancybox/Fancybox"
+
 
 const Footer = () => {
+  const [fancyboxRef] = useFancybox({
+    // Your custom options
+  });
   return (
     <>
       <footer className="footer-sec">
@@ -18,37 +23,48 @@ const Footer = () => {
             Back To Top
           </a>
         </div>
-        <div className={`container ${FooterCSS.footerBottom}`}>
-          <div className={` ${FooterCSS.bottomwrpr}`}>
-            <div className="row">
-              <div className="col-50">
-                <div className="links">
-                  <div className={`${FooterCSS.footerLinks}`}>
-                    <a href="">
-                      <span>
-                        <FaGithub />
-                      </span>
-                    </a>
-                    <a href="">
-                      <span>
-                        <BsInstagram />
-                      </span>
-                    </a>
-                    <a href="">
-                      <span>
-                        <FaLinkedin />
-                      </span>
-                    </a>
+        <div className="footer-bottom">
+          <div className={`container ${FooterCSS.footerBottom}`}>
+            <div className={` ${FooterCSS.bottomwrpr}`}>
+              <div className="row">
+                <div className="col-50">
+                  <div className="links">
+                    <div className={`${FooterCSS.footerLinks}`}>
+                      <a href="">
+                        <span>
+                          <FaGithub />
+                        </span>
+                      </a>
+                      <a href="">
+                        <span>
+                          <BsInstagram />
+                        </span>
+                      </a>
+                      <a href="">
+                        <span>
+                          <FaLinkedin />
+                        </span>
+                      </a>
+                    </div>
+                    <ul className={`${FooterCSS.footerInLinks}`}>
+                      <li><a href="">Home</a></li>
+                      <li><a href="">About</a></li>
+                      <li><a href="">Projects</a></li>
+                      <li><a href="">Journal</a></li>
+                    </ul>
                   </div>
-                  <div className={`${FooterCSS.footerInLinks}`}>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
+                </div>
+                <div className="col-50">
+                  <div ref={fancyboxRef}>
+                    <a
+                      href="https://www.google.com/maps/search/?api=1&query=Westminster+Bridge%2C+London&query_place_id=ChIJoaH8k8YEdkgRnmDd5iuUl9Y"
+                      data-fancybox
+                    >
+                      Launch Map
+                    </a>
                   </div>
                 </div>
               </div>
-              <div className="col-50"></div>
             </div>
           </div>
         </div>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,13 +3,12 @@ import Banner from "../components/Banner";
 import Myprojects from "../components/Myprojects";
 import Grid from "../components/Grid";
 
-
 const App = () => {
   return (
     <>
       <Banner />
       <Myprojects />
-      <Grid/>
+      <Grid />
     </>
   );
 };
